ffdaa19396d2d51f72594a4ff2e5b83418fd98bc
Simon de la Rouviere <simon@delarouviere.com>
2012-06-05 19:33:09 +0200

    Quick variable change

--- app/views.py
+++ app/views.py
@@ -4,7 +4,7 @@ from app import tasks
 
 def test_celery(request):
 	result = tasks.sleeptask.delay(10)
-	filter_one = tasks.sleeptask.delay(10)
-	filter_two = tasks.sleeptask.delay(10)
+	result_one = tasks.sleeptask.delay(10)
+	result_two = tasks.sleeptask.delay(10)
 	return HttpResponse(result.task_id)
 
