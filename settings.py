8a6098dab7e1b6e8cd63d8962c07aacc41d238fd
Simon de la Rouviere <simon@delarouviere.com>
2013-02-03 18:48:37 +0200

    Changed celery_test.urls to just urls, so that if it is cloned to a different folder name, it doesn't fail give an error.

--- settings.py
+++ settings.py
@@ -108,7 +108,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'celery_test.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
